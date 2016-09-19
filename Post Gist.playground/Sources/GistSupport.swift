460c4846bce8f745c3cb371cd0c09ac16d71762c
Kam Dahlin <kilodelta@kilodelta-mbp.local>
2016-09-19 11:46:28 -0700

    Added a postGist() function to the PlaygroundPage extension. This is the only public interface to posting the page now, which provides the line number for the callsite so that line can be removed from the text of the playground page prior to posting. Note: Looks like trying to post from the mac/ios sim isn't possible with the current API. Everything is run from a sandbox and access to the Contents.swift file isn't possible with out copy / paste. *sigh*

--- Post Gist.playground/Sources/GistSupport.swift
+++ Post Gist.playground/Sources/GistSupport.swift
@@ -3,39 +3,50 @@ import Foundation
 import PlaygroundSupport
 
 public extension PlaygroundPage {
-    #if (arch(i386) || arch(x86_64)) && os(iOS)
     
+    #if (arch(i386) || arch(x86_64)) && os(iOS)
     // PlaygroundPage doesn't have a text property on sim or OS X
+    // so we try to read out the file from the temp path and use its contents
     var text: String {
-    //TODO: need a different way to get this info for sim or OS X.
-        return "Some text from the playground"
+        print("------ can't post playground gists from mac based Playgrounds -----")
+        return ""
     }
     #endif
     
     var gistableText: String {
-        // strip out the calling code from the playground page
-        let text = self.text
-        return text
-    }
-    
-    public func complete(withResult result: PostResult) {
-        #if (arch(i386) || arch(x86_64)) && os(iOS)
-            // just print out the result on the sim/macos
-        #else
-        switch result  {
-        case .error(let error):
-            print(error)
-            
-            break
-        case .result(let url):
-            print(url)
-            break
+        // TODO: strip out the calling code from the playground page
+        // Need a way to mark this line in the playground page.
+        var results = self.text.components(separatedBy: CharacterSet.newlines)
+        if results.count < self.postGistLineNumber! {
+            return ""
         }
-        #endif
         
-        self.finishExecution()
+        results.remove(at: self.postGistLineNumber!)
+        return results.joined()
     }
     
+    @nonobjc static var lineKey = "PostGistCallerLineKey"
+    private var postGistLineNumber: Int? {
+        set (newValue) {
+            objc_setAssociatedObject(self, &PlaygroundPage.lineKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+        
+        get {
+            return objc_getAssociatedObject(self, &PlaygroundPage.lineKey) as? Int
+        }
+    }
+    
+    public func postGist(withDescription description: String, fileName: String, configuration: PostConfiguration, line: Int = #line, completion: @escaping (PostResult) -> Void) {
+        self.needsIndefiniteExecution = true
+        self.postGistLineNumber = line
+        
+        let gist = Gist(description: description, fileName: fileName, configuration: configuration)
+        gist.post(page: self) { (result) in
+            completion(result)
+            
+            self.finishExecution()
+        }
+    }
 }
 
 public enum PostResult {
@@ -45,6 +56,7 @@ public enum PostResult {
 
 public enum PostError: Error {
     case unknown
+    case noPlaygroundData
 }
 
 public enum PostConfiguration {
@@ -54,7 +66,7 @@ public enum PostConfiguration {
     case user(oauthToken:String, isPublic:Bool)
 }
 
-public struct Gist {
+struct Gist {
     let url: URL
     let description: String
     let fileName: String
@@ -82,25 +94,29 @@ public struct Gist {
         return request
     }
     
-    public init(withURL url: URL, _ description: String, _ fileName: String, _ configuration: PostConfiguration) {
+    init(withURL url: URL, _ description: String, _ fileName: String, _ configuration: PostConfiguration) {
         self.url = url
         self.description = description
         self.fileName = fileName
         self.configuration = configuration
     }
     
-    public init(withURLString string: String = "https://api.github.com/gists", description: String, fileName: String, configuration: PostConfiguration) {
+    init(withURLString string: String = "https://api.github.com/gists", description: String, fileName: String, configuration: PostConfiguration) {
         self.init(withURL: URL(string: string)!, description, fileName, configuration)
     }
     
-    public func post(page: PlaygroundPage, completion: @escaping (PostResult) -> Void) {
-        let json = self.json(withPath: page)
+    func post(page: PlaygroundPage, completion: @escaping (PostResult) -> Void) {
+        guard let json = self.json(withPath: page) else {
+            completion(PostResult.error(PostError.noPlaygroundData))
+            return
+        }
+        
         var request = self.urlRequest
         request.httpBody = json
         
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
             let errorResult: (Error?) -> PostResult = { (error) in
-               return PostResult.error(error ?? PostError.unknown)
+                return PostResult.error(error ?? PostError.unknown)
             }
             
             guard let response = response as? HTTPURLResponse else {
@@ -136,13 +152,17 @@ public struct Gist {
     }
     
     private func json(withPath page: PlaygroundPage) -> Data? {
+        guard !page.gistableText.isEmpty else {
+            return nil
+        }
+        
         let textPayload = page.gistableText
         let json: [String : Any] = ["description" : self.description, "public" : self.isPublic, "files" : [self.fileName : ["content" : textPayload]]]
         guard let data = try? JSONSerialization.data(withJSONObject: json, options: []) else {
             print("failed to convert json")
             return nil
         }
-
+        
         return data
     }
     
